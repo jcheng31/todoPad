d2ceac3e3d834c16e08b7840dcba2ba41e7294f0
Jerome Cheng <jcheng31@gmail.com>
2013-05-01 16:02:16 +0800

    Add a event handler for the textbox's TextChanged event.

--- TodoPad/Views/MainWindow.xaml.cs
+++ TodoPad/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
 
 namespace TodoPad.Views
 {
@@ -10,6 +13,17 @@ namespace TodoPad.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DocumentBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            RichTextBox textBox = (RichTextBox) sender;
+            foreach (Paragraph currentLine in textBox.Document.Blocks)
+            {
+                TextRange currentLineRange = new TextRange(currentLine.ContentStart, currentLine.ContentEnd);
+                String currentLineText = currentLineRange.Text;
+
+            }
         }
     }
 }
