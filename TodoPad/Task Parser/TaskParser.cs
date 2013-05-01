369221a80b19651acaa0a7bfe217294e4553fc2d
Jerome Cheng <jcheng31@gmail.com>
2013-05-01 16:02:50 +0800

    Add Task Parser class.

---
+++ TodoPad/Task Parser/TaskParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace TodoPad.Task_Parser
+{
+    class TaskParser
+    {
+        public static void ParseTask(TextRange taskRange)
+        {
+            TextPointer pointer = taskRange.Start;
+        }
+    }
+}
