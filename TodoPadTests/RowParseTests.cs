08fddf903ec9b17e7be7812c281c5ca3bc1237e3
Jerome Cheng <jcheng31@gmail.com>
2013-05-10 17:44:39 +0800

    Add tests.

--- TodoPadTests/RowParseTests.cs
+++ TodoPadTests/RowParseTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TodoPad.Models;
 
 namespace TodoPadTests
 {
@@ -7,8 +8,34 @@ namespace TodoPadTests
     public class RowParseTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void BasicParseTest()
         {
+            Row testRow = new Row("Initial version of +Todopad @Coding");
+            Assert.IsNotNull(testRow, "Row shouldn't be null.");
+
+            Assert.IsTrue(testRow.HasContexts, "Row should have contexts.");
+            Assert.AreEqual(testRow.Contexts[0], "+Todopad", "Row should have correct context.");
+            Assert.IsTrue(testRow.HasProjects, "Row should have projects.");
+            Assert.AreEqual(testRow.Projects[0], "@Coding", "Row should have correct project.");
+        }
+
+        [TestMethod]
+        public void InvalidParseTest()
+        {
+            Row testRow = new Row("");
+            Assert.IsFalse(testRow.HasText, "Row shouldn't have text.");
+            Assert.IsFalse(testRow.HasContexts, "Row shouldn't have contexts.");
+            Assert.IsFalse(testRow.HasProjects, "Row shouldn't have projects.");
+        }
+
+        [TestMethod]
+        public void CompletedParseTest()
+        {
+            Row testRow = new Row("x 2013-04-19 (B) due:2013-04-20 +CS3241 Lab 5 @Coding +University");
+            Assert.IsTrue(testRow.IsCompleted, "Row should be completed.");
+            Assert.IsTrue(testRow.HasContexts, "Row should have contexts.");
+            Assert.IsTrue(testRow.HasProjects, "Row should have projects.");
+            Assert.IsTrue(testRow.HasPriority, "Row should have priority.");
         }
     }
 }
