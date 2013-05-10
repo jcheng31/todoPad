a26d1c597c61c39d16fa1c087b358f7050f8f779
Jerome Cheng <jcheng31@gmail.com>
2013-05-10 17:43:54 +0800

    Improve parsing of completed files.

--- TodoPad/Models/Row.cs
+++ TodoPad/Models/Row.cs
@@ -57,11 +57,11 @@ namespace TodoPad.Models
 
         public Row(String task)
         {
-            if (task == "") return;
-
             RowText = task;
 
             String[] components = task.Split();
+
+            Regex priorityRegex;
 
             // Check if the task is completed.
             const string dateFormat = "yyyy-MM-dd";
@@ -69,60 +69,74 @@ namespace TodoPad.Models
             {
                 // There must be a completion date.
                 // (If there isn't, the task isn't properly formatted.)
-                bool isCompletionPresent = DateTime.TryParseExact(components[1], dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out CompletionDate);
+                bool isCompletionPresent = DateTime.TryParseExact(components[1], dateFormat,
+                                                                  CultureInfo.InvariantCulture, DateTimeStyles.None,
+                                                                  out CompletionDate);
                 if (isCompletionPresent)
                 {
                     CompletionDateRange = new Tuple<int, int>(2, 12);
                 }
+
+                priorityRegex = new Regex("\\s\\([A-Z]\\)");
             }
             else
             {
-                // Check if there's a priority.
-                Regex priorityRegex = new Regex("^\\([A-Z]\\)");
-                Match priorityMatch = priorityRegex.Match(task);
-                if (priorityMatch.Success)
+                priorityRegex = new Regex("^\\([A-Z]\\)");
+            }
+
+            // Check if there's a priority.
+            Match priorityMatch = priorityRegex.Match(task);
+            if (priorityMatch.Success)
+            {
+                PriorityRange = new Tuple<int, int>(priorityMatch.Index, priorityMatch.Index + priorityMatch.Length);
+            }
+
+            // Check for a creation date. If we have a completion date,
+            // we use the second date found; otherwise, we just use the first.
+            Regex dateRegex = new Regex("\\d{4}-[0|1][0-9]-[0-3][1-9]");
+            MatchCollection dateMatches = dateRegex.Matches(task);
+            Match creationMatch;
+            if (dateMatches.Count > 0)
+            {
+                if (IsCompleted && dateMatches.Count > 1)
                 {
-                    PriorityRange = new Tuple<int, int>(priorityMatch.Index, priorityMatch.Index + priorityMatch.Length);
+                    creationMatch = dateMatches[1];
                 }
-
-                // Check for a creation date. We just match the first date 
-                // (since a creation date is supposed to come immediately after 
-                // priority, or is the first thing on the line.
-                Regex dateRegex = new Regex("\\d{4}-[0|1][0-9]-[0-3][1-9]");
-                Match creationMatch = dateRegex.Match(task);
-                if (creationMatch.Success)
+                else
                 {
-                    CreationDateRange = new Tuple<int, int>(creationMatch.Index, creationMatch.Index + creationMatch.Length);
-                    DateTime.TryParseExact(task.Substring(creationMatch.Index, creationMatch.Length), dateFormat,
-                                           CultureInfo.InvariantCulture, DateTimeStyles.None, out CreationDate);
+                    creationMatch = dateMatches[0];
                 }
+                
+                CreationDateRange = new Tuple<int, int>(creationMatch.Index, creationMatch.Index + creationMatch.Length);
+                DateTime.TryParseExact(task.Substring(creationMatch.Index, creationMatch.Length), dateFormat,
+                                       CultureInfo.InvariantCulture, DateTimeStyles.None, out CreationDate);
+            }
 
-                // Check if we have any projects/contexts.
-                Contexts = new List<string>();
-                ContextRanges = new List<Tuple<int, int>>();
-                Projects = new List<string>();
-                ProjectRanges = new List<Tuple<int, int>>();
+            // Check if we have any projects/contexts.
+            Contexts = new List<string>();
+            ContextRanges = new List<Tuple<int, int>>();
+            Projects = new List<string>();
+            ProjectRanges = new List<Tuple<int, int>>();
 
-                Regex projectRegex = new Regex("@\\w+"); // Match any word beginning with "@"
-                Regex contextRegex = new Regex("\\+\\w+"); // Match any word beginning with "+";
+            Regex projectRegex = new Regex("@\\w+"); // Match any word beginning with "@"
+            Regex contextRegex = new Regex("\\+\\w+"); // Match any word beginning with "+";
 
-                foreach (Match match in projectRegex.Matches(task))
-                {
-                    string project = match.Value;
-                    Tuple<int, int> indices = new Tuple<int, int>(match.Index, match.Index + match.Length);
+            foreach (Match match in projectRegex.Matches(task))
+            {
+                string project = match.Value;
+                Tuple<int, int> indices = new Tuple<int, int>(match.Index, match.Index + match.Length);
 
-                    Projects.Add(project);
-                    ProjectRanges.Add(indices);
-                }
+                Projects.Add(project);
+                ProjectRanges.Add(indices);
+            }
 
-                foreach (Match match in contextRegex.Matches(task))
-                {
-                    string context = match.Value;
-                    Tuple<int, int> indices = new Tuple<int, int>(match.Index, match.Index + match.Length);
+            foreach (Match match in contextRegex.Matches(task))
+            {
+                string context = match.Value;
+                Tuple<int, int> indices = new Tuple<int, int>(match.Index, match.Index + match.Length);
 
-                    Contexts.Add(context);
-                    ContextRanges.Add(indices);
-                }
+                Contexts.Add(context);
+                ContextRanges.Add(indices);
             }
         }
     }
