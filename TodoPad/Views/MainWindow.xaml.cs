10b746d6a45ac5b36251fb6c4872309a6e7ae246
Jerome Cheng <jcheng31@gmail.com>
2013-05-11 15:12:56 +0800

    Hook up MainWindow to TaskParser.

--- TodoPad/Views/MainWindow.xaml.cs
+++ TodoPad/Views/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using TodoPad.Models;
 using TodoPad.Task_Parser;
 
 namespace TodoPad.Views
@@ -18,25 +20,27 @@ namespace TodoPad.Views
 
         private void DocumentBoxTextChanged(object sender, TextChangedEventArgs e)
         {
-            RichTextBox textBox = (RichTextBox) sender;
-            
+            RichTextBox textBox = (RichTextBox)sender;
+
             // Remove this handler so we don't trigger it when formatting.
             textBox.TextChanged -= DocumentBoxTextChanged;
 
-            Paragraph currentLine = textBox.Document.Blocks.FirstBlock as Paragraph;
-            while (currentLine != null)
+            Paragraph currentParagraph = textBox.Document.Blocks.FirstBlock as Paragraph;
+            while (currentParagraph != null)
             {
-                TextRange currentLineRange = new TextRange(currentLine.ContentStart, currentLine.ContentEnd);
-                TaskParser.ParseTask(currentLineRange);
+                // Get the text on this row.
+                TextPointer start = currentParagraph.ContentStart;
+                TextPointer end = currentParagraph.ContentEnd;
+                TextRange currentTextRange = new TextRange(start, end);
 
-                currentLine = currentLine.NextBlock as Paragraph;
+                // Parse the row.
+                Row currentRow = new Row(currentTextRange.Text);
+
+                // Format the displayed text.
+                TaskParser.FormatTextRange(currentTextRange, currentRow);
+
+                currentParagraph = currentParagraph.NextBlock as Paragraph;
             }
-
-            //foreach (Paragraph currentLine in textBox.Document.Blocks)
-            //{
-            //    TextRange currentLineRange = new TextRange(currentLine.ContentStart, currentLine.ContentEnd);
-            //    TaskParser.ParseTask(currentLineRange);
-            //}
 
             // Restore this handler.
             textBox.TextChanged += DocumentBoxTextChanged;
