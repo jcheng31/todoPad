f1b23406ae2f4649c2654606a8e741d4c14c1d7f
Jerome Cheng <jcheng31@gmail.com>
2013-05-12 15:47:40 +0800

    Implement TaskFile constructor.

--- TodoPad/Models/TaskFile.cs
+++ TodoPad/Models/TaskFile.cs
@@ -8,5 +8,22 @@ namespace TodoPad.Models
 {
     class TaskFile
     {
+        public String Path { get; set; }
+        public List<Row> Rows { get; set; }
+
+        public TaskFile(String path, String contents)
+        {
+            Path = path;
+
+            String[] lines = contents.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+
+            Rows = new List<Row>(lines.Length);
+
+            foreach (string currentLine in lines)
+            {
+                Row currentRow = new Row(currentLine);
+                Rows.Add(currentRow);
+            }
+        }
     }
 }
