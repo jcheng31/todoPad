7c03a69e4d7977972b976af4a14b5dba0aae85b7
Jerome Cheng <jcheng31@gmail.com>
2013-05-13 12:14:30 +0800

    Change TaskFile to store rows as Strings.

--- TodoPad/Models/TaskFile.cs
+++ TodoPad/Models/TaskFile.cs
@@ -9,21 +9,12 @@ namespace TodoPad.Models
     class TaskFile
     {
         public String Path { get; set; }
-        public List<Row> Rows { get; set; }
+        public String[] Rows { get; set; }
 
         public TaskFile(String path, String contents)
         {
             Path = path;
-
-            String[] lines = contents.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-
-            Rows = new List<Row>(lines.Length);
-
-            foreach (string currentLine in lines)
-            {
-                Row currentRow = new Row(currentLine);
-                Rows.Add(currentRow);
-            }
+            Rows = contents.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
         }
     }
 }
