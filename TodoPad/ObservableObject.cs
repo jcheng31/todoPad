2a54e7f7ac85ec3ffa03db7a6f8b8b635047c37f
Jerome Cheng <jcheng31@gmail.com>
2013-05-27 22:03:39 +0800

    Add ObservableObject and SettingsViewModel files.

---
+++ TodoPad/ObservableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace TodoPad.Models
+{
+    public class ObservableObject : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        protected virtual void OnPropertyChanging(string propertyName)
+        {
+            PropertyChangingEventHandler handler = PropertyChanging;
+            if (handler != null) handler(this, new PropertyChangingEventArgs(propertyName));
+        }
+    }
+}
