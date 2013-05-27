ef2983dd699cfedf6224872c1de0a332205516fc
Jerome Cheng <jcheng31@gmail.com>
2013-05-27 22:04:31 +0800

    Basic SettingsViewModel implementation.

--- TodoPad/ViewModels/SettingsViewModel.cs
+++ TodoPad/ViewModels/SettingsViewModel.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace TodoPad.ViewModels
 {
-    class SettingsViewModel
+    class SettingsViewModel : ObservableObject
     {
+        public List<FontFamily> FontFamilies { get; set; }
+
+        public SettingsViewModel()
+        {
+            InstalledFontCollection fonts = new InstalledFontCollection();
+            FontFamilies = fonts.Families;
+        }
     }
 }
